b57bc8c0968ed95d148394880450d17e211b466b
KofinasApostolos <a.kofinas@informlykos.com>
2023-12-21 10:58:09 +0200

    Working insert and validation

--- Program.cs
+++ Program.cs
@@ -10,96 +10,125 @@ class Program
     static void Main()
     {
         var path = "C:\\Users\\kofinasa\\Desktop\\Academic_ID_650437_20231213114227.csv";
-
-        using var reader = new StreamReader(path);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-
-        csv.Read();
-        csv.ReadHeader();
-
-
+        var tableName = "AcademicIDs";
         var connectionString = "Data Source=DM-07-PC\\TEST24;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
         connectionString = "Data Source=DM-07-PC\\TEST24;Integrated Security=True;Connect Timeout=30;Encrypt=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         connectionString = "Data Source=DM-07-PC\\TEST24;Integrated Security=True;Connect Timeout=30;Encrypt=False;Multi Subnet Failover=False";
         connectionString = "Data Source=DM-07-PC\\TEST24;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
 
+        List<AcademicId> listOfIds = new List<AcademicId>();
+        using (StreamReader reader1 = new StreamReader(path))
+        {
+            while (!reader1.EndOfStream)
+            {
+                string line = reader1.ReadLine();
+                string[] splitedValues = line.Split(';');
 
+                AcademicId academicId = new AcademicId
+                {
+                    ChipSerialNumber = GetValueOrDefault(splitedValues, 0),
+                    Pk1Rsa3072 = GetValueOrDefault(splitedValues, 1),
+                    FpSha256Pk1Rsa3072 = GetValueOrDefault(splitedValues, 2),
+                    Pk2Rsa3072 = GetValueOrDefault(splitedValues, 3),
+                    FpSha256Pk2Rsa3072 = GetValueOrDefault(splitedValues, 4),
+                    Pk1Rsa4096 = GetValueOrDefault(splitedValues, 5),
+                    FpSha256Pk1Rsa4096 = GetValueOrDefault(splitedValues, 6),
+                    Pk1EcdsaSecp256r1 = GetValueOrDefault(splitedValues, 7),
+                    FpSha256Pk1EcdsaSecp256r1 = GetValueOrDefault(splitedValues, 8),
+                    Pk2EcdsaSecp256r1 = GetValueOrDefault(splitedValues, 9),
+                    FpSha256Pk2EcdsaSecp256r1 = GetValueOrDefault(splitedValues, 10),
+                    Pk3EcdsaSecp256r1 = GetValueOrDefault(splitedValues, 11),
+                    FpSha256Pk3EcdsaSecp256r1 = GetValueOrDefault(splitedValues, 12),
+                    Pk1EcdsaSecp384r1 = GetValueOrDefault(splitedValues, 13),
+                    FpSha256Pk1EcdsaSecp384r1 = GetValueOrDefault(splitedValues, 14),
+                    Pin = GetValueOrDefault(splitedValues, 15),
+                    Puk = GetValueOrDefault(splitedValues, 16),
+                };
+
+                listOfIds.Add(academicId);
+            }
+        }
 
         try
         {
-            using var connection = new SqlConnection(connectionString);
-            connection.Open();
+            List<AcademicId> existingIds = new List<AcademicId>();
+            List<AcademicId> nonExistingIds = new List<AcademicId>();
 
-            var tableName = "AcademicIDs";
-            var sqlCommand = $"INSERT INTO {tableName} VALUES (@ChipSerialNumber, @Pk1Rsa3072, @FpSha256Pk1Rsa3072, @Pk2Rsa3072, @FpSha256Pk2Rsa3072, " +
-                              "@Pk1Rsa4096, @FpSha256Pk1Rsa4096, @Pk1EcdsaSecp256r1, @FpSha256Pk1EcdsaSecp256r1, @Pk2EcdsaSecp256r1, @FpSha256Pk2EcdsaSecp256r1, " +
-                              "@Pk3EcdsaSecp256r1, @FpSha256Pk3EcdsaSecp256r1, @Pk1EcdsaSecp384r1, @FpSha256Pk1EcdsaSecp384r1, @Pin, @Puk)";
-
-            var listOfAcademicIds = new List<AcademicId>();
-
-            while (csv.Read())
+            using (SqlConnection connection1 = new SqlConnection(connectionString))
             {
-                var firstline = csv.GetField<string>(0);                
-                var linesSplited = firstline.Split(';');
+                connection1.Open();
 
-                foreach (var line in linesSplited)
+
+                foreach (var academicId in listOfIds)
                 {
-                    Console.WriteLine($"Line: {line}");
-                    // Process each line as needed
+                    var chipSerialNumber = academicId.ChipSerialNumber;
+                    var query = $"SELECT COUNT(1) FROM {tableName} WHERE ChipSerialNumber = @ChipSerialNumber";
+
+                    using (SqlCommand command = new SqlCommand(query, connection1))
+                    {
+                        command.Parameters.AddWithValue("@ChipSerialNumber", chipSerialNumber);
+                        var count = (int)command.ExecuteScalar();
+
+                        if (count > 0)
+                        {
+                            existingIds.Add(academicId);
+                        }
+                        else
+                        {
+                            nonExistingIds.Add(academicId);
+                        }
+                    }
                 }
-
-                Console.WriteLine();  // Separate lines for clarity
-                
+            }
 
 
-                var academicId = new AcademicId
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                foreach (var academicId in nonExistingIds)
                 {
-                    ChipSerialNumber = csv.GetField<string>(0),
-                    Pk1Rsa3072 = csv.GetField<string>(1),
-                    FpSha256Pk1Rsa3072 = csv.GetField<string>(2),
-                    Pk2Rsa3072 = csv.GetField<string>(3),
-                    FpSha256Pk2Rsa3072 = csv.GetField<string>(4),
-                    Pk1Rsa4096 = csv.GetField<string>(5),
-                    FpSha256Pk1Rsa4096 = csv.GetField<string>(6),
-                    Pk1EcdsaSecp256r1 = csv.GetField<string>(7),
-                    FpSha256Pk1EcdsaSecp256r1 = csv.GetField<string>(8),
-                    Pk2EcdsaSecp256r1 = csv.GetField<string>(9),
-                    FpSha256Pk2EcdsaSecp256r1 = csv.GetField<string>(10),
-                    Pk3EcdsaSecp256r1 = csv.GetField<string>(11),
-                    FpSha256Pk3EcdsaSecp256r1 = csv.GetField<string>(12),
-                    Pk1EcdsaSecp384r1 = csv.GetField<string>(13),
-                    FpSha256Pk1EcdsaSecp384r1 = csv.GetField<string>(14),
-                    Pin = csv.GetField<string>(15),
-                    Puk = csv.GetField<string>(16)
-                };
+                    var sqlCommand = $"INSERT INTO {tableName} " +
+                                      "VALUES (@ChipSerialNumber, @Pk1Rsa3072, @FpSha256Pk1Rsa3072, @Pk2Rsa3072, @FpSha256Pk2Rsa3072, " +
+                                      "@Pk1Rsa4096, @FpSha256Pk1Rsa4096, @Pk1EcdsaSecp256r1, @FpSha256Pk1EcdsaSecp256r1, @Pk2EcdsaSecp256r1, @FpSha256Pk2EcdsaSecp256r1, " +
+                                      "@Pk3EcdsaSecp256r1, @FpSha256Pk3EcdsaSecp256r1, @Pk1EcdsaSecp384r1, @FpSha256Pk1EcdsaSecp384r1, @Pin, @Puk)";
 
-                using var command = new SqlCommand(sqlCommand, connection);
-                command.Parameters.AddWithValue("@ChipSerialNumber", academicId.ChipSerialNumber);
-                command.Parameters.AddWithValue("@Pk1Rsa3072", academicId.Pk1Rsa3072);
-                command.Parameters.AddWithValue("@FpSha256Pk1Rsa3072", academicId.FpSha256Pk1Rsa3072);
-                command.Parameters.AddWithValue("@Pk2Rsa3072", academicId.Pk2Rsa3072);
-                command.Parameters.AddWithValue("@FpSha256Pk2Rsa3072", academicId.FpSha256Pk2Rsa3072);
-                command.Parameters.AddWithValue("@Pk1Rsa4096", academicId.Pk1Rsa4096);
-                command.Parameters.AddWithValue("@FpSha256Pk1Rsa4096", academicId.FpSha256Pk1Rsa4096);
-                command.Parameters.AddWithValue("@Pk1EcdsaSecp256r1", academicId.Pk1EcdsaSecp256r1);
-                command.Parameters.AddWithValue("@FpSha256Pk1EcdsaSecp256r1", academicId.FpSha256Pk1EcdsaSecp256r1);
-                command.Parameters.AddWithValue("@Pk2EcdsaSecp256r1", academicId.Pk2EcdsaSecp256r1);
-                command.Parameters.AddWithValue("@FpSha256Pk2EcdsaSecp256r1", academicId.FpSha256Pk2EcdsaSecp256r1);
-                command.Parameters.AddWithValue("@Pk3EcdsaSecp256r1", academicId.Pk3EcdsaSecp256r1);
-                command.Parameters.AddWithValue("@FpSha256Pk3EcdsaSecp256r1", academicId.FpSha256Pk3EcdsaSecp256r1);
-                command.Parameters.AddWithValue("@Pk1EcdsaSecp384r1", academicId.Pk1EcdsaSecp384r1);
-                command.Parameters.AddWithValue("@FpSha256Pk1EcdsaSecp384r1", academicId.FpSha256Pk1EcdsaSecp384r1);
-                command.Parameters.AddWithValue("@Pin", academicId.Pin);
-                command.Parameters.AddWithValue("@Puk", academicId.Puk);
+                    using (SqlCommand command = new SqlCommand(sqlCommand, connection))
+                    {
+                        command.Parameters.AddWithValue("@ChipSerialNumber", academicId.ChipSerialNumber);
+                        command.Parameters.AddWithValue("@Pk1Rsa3072", academicId.Pk1Rsa3072);
+                        command.Parameters.AddWithValue("@FpSha256Pk1Rsa3072", academicId.FpSha256Pk1Rsa3072);
+                        command.Parameters.AddWithValue("@Pk2Rsa3072", academicId.Pk2Rsa3072);
+                        command.Parameters.AddWithValue("@FpSha256Pk2Rsa3072", academicId.FpSha256Pk2Rsa3072);
+                        command.Parameters.AddWithValue("@Pk1Rsa4096", academicId.Pk1Rsa4096);
+                        command.Parameters.AddWithValue("@FpSha256Pk1Rsa4096", academicId.FpSha256Pk1Rsa4096);
+                        command.Parameters.AddWithValue("@Pk1EcdsaSecp256r1", academicId.Pk1EcdsaSecp256r1);
+                        command.Parameters.AddWithValue("@FpSha256Pk1EcdsaSecp256r1", academicId.FpSha256Pk1EcdsaSecp256r1);
+                        command.Parameters.AddWithValue("@Pk2EcdsaSecp256r1", academicId.Pk2EcdsaSecp256r1);
+                        command.Parameters.AddWithValue("@FpSha256Pk2EcdsaSecp256r1", academicId.FpSha256Pk2EcdsaSecp256r1);
+                        command.Parameters.AddWithValue("@Pk3EcdsaSecp256r1", academicId.Pk3EcdsaSecp256r1);
+                        command.Parameters.AddWithValue("@FpSha256Pk3EcdsaSecp256r1", academicId.FpSha256Pk3EcdsaSecp256r1);
+                        command.Parameters.AddWithValue("@Pk1EcdsaSecp384r1", academicId.Pk1EcdsaSecp384r1);
+                        command.Parameters.AddWithValue("@FpSha256Pk1EcdsaSecp384r1", academicId.FpSha256Pk1EcdsaSecp384r1);
+                        command.Parameters.AddWithValue("@Pin", academicId.Pin);
+                        command.Parameters.AddWithValue("@Puk", academicId.Puk);
 
-                command.ExecuteNonQuery();
+                        command.ExecuteNonQuery();
+                    }
+                }
             }
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
         }
+    }
+
+    public static string GetValueOrDefault(string[] values, int index)
+    {
+        return index >= 0 && index < values.Length && values[index].Length > 0
+            ? values[index]
+            : null;
     }
 
     public class AcademicId
